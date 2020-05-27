d06e3494bc3564e6772a15d45852c3ab48b60be3
Sai Kumar Thallada <thallada_saikumar@yahoo.com>
2020-05-26 20:58:21 -0400

    Updated with ESlint and prettier reference link

--- JavaScript/NodeJs/reference.js
+++ JavaScript/NodeJs/reference.js
@@ -1,14 +1,14 @@
 /*############ Different ways to Export Modules ###############*/
 
 const getName = () => {
-    return 'Jim';
+  return "Jim";
 };
 
 const getLocation = () => {
-  return 'Munich';
+  return "Munich";
 };
 
-const dateOfBirth = '12.01.1982';
+const dateOfBirth = "12.01.1982";
 
 // Type-1:
 module.exports.getName = getName;
@@ -17,44 +17,48 @@ module.exports.dob = dateOfBirth;
 
 //Type-2:
 module.exports = {
-    getName,
-    getLocation,
-    dateOfBirth
-}
-
+  getName,
+  getLocation,
+  dateOfBirth,
+};
 
 /*############ Different ways to Import Modules ###############*/
 // Type:1
-const reference = require('./reference')
+const reference = require("./reference");
 
 // Type:2
 // Destructuring assignment, we can cherry-pick what we want to import:
-const { getName, dob } = require('./reference');
-
+const { getName, dob } = require("./reference");
 
 /*############ Custom Events with EventEmitter ###############*/
-const events = require('events');
+const events = require("events");
 const emitter = new events.EventEmitter();
 
-emitter.on('customEvent', (user, message) => {
-    console.log(` ${user} : ${message} `);
+emitter.on("customEvent", (user, message) => {
+  console.log(` ${user} : ${message} `);
 });
 
-emitter.emit('customEvent', 'Computer', 'Hello World');
-emitter.emit('customEvent', 'Alex', 'Awesome this helped me in knowing emitter!!');
-
+emitter.emit("customEvent", "Computer", "Hello World");
+emitter.emit(
+  "customEvent",
+  "Alex",
+  "Awesome this helped me in knowing emitter!!"
+);
 
 /*############ Creating a child process with exec/spawn ###############*/
-const cp = require('child_process');
+const cp = require("child_process");
 
 // 'exec' function to execute external system commands in Sync
 // 'spawn' function to execute external system commands in Async
-cp.exec('open https://www.linkedin.com/in/thalladasaikumar');
+cp.exec("open https://www.linkedin.com/in/thalladasaikumar");
 
-cp.exec('ls', (err, data)=>{
-    if(err){
-        throw err;
-    }
-    console.log(data);
-})
+cp.exec("ls", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log(data);
+});
 
+/*############ ESLint and prettier for code formatting in VSCode and auto save ###############*/
+// Watch he below video gives you better idea
+// https://www.linkedin.com/learning/building-a-website-with-node-js-and-express-js-3/setting-up-eslint-and-prettier?autoplay=true&pathUrn=urn%3Ali%3AlyndaLearningPath%3A5b32b6d5498e4ef39c04c55c
