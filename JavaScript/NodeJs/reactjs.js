3f9c5e2ec36b9b8f7f1eca7c0fbe4dde6278f28d
Sai Kumar Thallada <thallada_saikumar@yahoo.com>
2020-05-28 00:42:42 -0400

    Added reactjs file

---
+++ JavaScript/NodeJs/reactjs.js
@@ -0,0 +1,5 @@
+// Useful guide for Node and React -> https://jscomplete.com/reactful
+
+/* to install react: npm install react react-dom
+- We use the React library to describe our user interfaces, react-dom library to render those interfaces on both the front-end and the back-end.
+*/
